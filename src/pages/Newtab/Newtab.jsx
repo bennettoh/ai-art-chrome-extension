ab7848068d18b77ff6c7c4a78f1215263364c332
Bennett Oh <bennettoh.dev@gmail.com>
2022-07-16 14:35:16 -0700

    clean up and fix scss error

--- src/pages/Newtab/Newtab.jsx
+++ src/pages/Newtab/Newtab.jsx
@@ -1,7 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import logo from '../../assets/img/logo.svg';
-import './Newtab.css';
-import './Newtab.scss';
 import { fetchGoogleSheets } from '../../utils';
 
 const url = `https://docs.google.com/spreadsheets/d/1t6ntSRGvFm22WLxwq2xgsOmnWlQEO148j13LX9-XQT0/gviz/tq?tqx=out:csv`;
@@ -16,19 +13,6 @@ const Newtab = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Newtab/Newtab.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-        <h6>The color of this paragraph is defined using SASS.</h6>
         {data.map((item, i) => (
           <a href={item.link} key={i}>
             <h1>{item.name}</h1>
